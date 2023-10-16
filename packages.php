5474515afc3d3a22b6bf506db97dba91e1f35c18
MehdiPG <80159231+MehdiPG@users.noreply.github.com>
2023-10-16 22:02:39 +0200

    Add files via upload

---
+++ packages.php
@@ -0,0 +1,15 @@
+<?php
+$keys = isset($_GET['key']) ? array_map('trim', explode(',', strtolower($_GET['key']))) : array();
+$packages = array();
+
+$packages[] = array(
+    'name' => 'Heteritos 4',
+    'title' => 'Heteritos 4',
+    'version' => '4.0.0',
+    'priority' => 0,
+    'location' => 'heteritos-4.json',
+);
+
+$out = array('minimumVersion' => 1, 'packages' => $packages);
+header('Content-Type: text/plain; charset=utf-8');
+echo json_encode($out);
